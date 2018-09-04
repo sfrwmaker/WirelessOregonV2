537f4a67140fda5576143a0d047eaa4b1117ad0e
sfrwmaker <sfrwmaker@gmail.com>
2018-09-04 19:52:29 +0300

    Add files via upload

--- examples/OregonReceiver/OregonReceiver.ino
+++ examples/OregonReceiver/OregonReceiver.ino
@@ -6,23 +6,30 @@
  */
 
 OregonDecoderV2 orscV2;
- 
+
 void setup () {
     Serial.begin(115200);                         // Enable Serial for packet DUMP output
     Serial.println("\n[ookDecoder]");
-    orscV2.begin(0);
+    orscV2.begin(digitalPinToInterrupt(4));
 }
- 
-void loop () {
 
-  int temp = 0;
-  byte humm = 0;
-  byte ch = 0;
-  byte id = 0;
-  bool batt = false;
-  if (orscV2.receiveData(ch, id, temp, humm, batt, true)) {
-    Serial.print("ID = ");     Serial.print(id, HEX);
-    Serial.print(", Temp = "); Serial.print(temp/10); Serial.print("."); Serial.print(temp%10);
-    Serial.print(", Humm = "); Serial.println(humm);
+void loop () {
+  uint8_t ch   = 0;
+  uint8_t id   = 0;
+  int16_t temp  = 0;
+  uint8_t humm = 0;
+  bool battOK = false;
+  if (orscV2.receiveData(ch, id, temp, humm, battOK)) {
+    Serial.print("Channel = "); Serial.print(id, HEX);
+    Serial.print(", ID = ");    Serial.print(id, HEX);
+    char minus = ' ';
+    if (temp < 0) {
+      minus = '-';
+      temp *= -1;
+    }
+    Serial.print(", Temp = ");  Serial.print(minus); Serial.print(temp/10); Serial.print("."); Serial.print(temp%10);
+    Serial.print(", Humm = ");  Serial.print(humm);
+    if (battOK) Serial.println(", battery OK");
+    else        Serial.println(", replace battery");
   }
 }
